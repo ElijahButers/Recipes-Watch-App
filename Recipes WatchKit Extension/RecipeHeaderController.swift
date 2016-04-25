b4053ce9a6a2e136ad4c26b409bf7c982e803b73
ElijahButers <Elijahbuters@gmail.com>
2016-04-25 23:51:26 +0300

    Added RecipeHeaderController, RecipeStepController, then added these new classes to two row controllers, set identifiers.

---
+++ Recipes WatchKit Extension/RecipeHeaderController.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeHeaderController.swift
+//  Recipes
+//
+//  Created by User on 4/25/16.
+//  Copyright © 2016 Ray Wenderlich. All rights reserved.
+//
+
+import WatchKit
+
+class RecipeHeaderController: NSObject {
+
+}
---
+++ Recipes WatchKit Extension/RecipeStepController.swift
@@ -0,0 +1,13 @@
+//
+//  RecipeStepController.swift
+//  Recipes
+//
+//  Created by User on 4/25/16.
+//  Copyright © 2016 Ray Wenderlich. All rights reserved.
+//
+
+import WatchKit
+
+class RecipeStepController: NSObject {
+
+}

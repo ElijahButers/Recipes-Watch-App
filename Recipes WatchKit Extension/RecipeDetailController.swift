03e24b0198c429c2a0347d16b2a051b8614da21e
ElijahButers <Elijahbuters@gmail.com>
2016-04-26 10:33:06 +0300

    Added a RecipeDetailController class, then added to second interface controller, connected table to RecipeDetailController as outlet.

---
+++ Recipes WatchKit Extension/RecipeDetailController.swift
@@ -0,0 +1,33 @@
+//
+//  RecipeDetailController.swift
+//  Recipes
+//
+//  Created by User on 4/26/16.
+//  Copyright © 2016 Ray Wenderlich. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class RecipeDetailController: WKInterfaceController {
+
+    @IBOutlet var table: WKInterfaceTable!
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}

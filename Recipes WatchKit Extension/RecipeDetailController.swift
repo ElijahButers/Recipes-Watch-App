b966d294d4f91adef68c56e50b7aafe23d519cad
ElijahButers <Elijahbuters@gmail.com>
2016-04-26 11:12:30 +0300

    Changed code in awakeWithContext method of RecipeDetailController that loads name of the recipe as well as all the cooking instructions.

--- Recipes WatchKit Extension/RecipeDetailController.swift
+++ Recipes WatchKit Extension/RecipeDetailController.swift
@@ -19,13 +19,15 @@ class RecipeDetailController: WKInterfaceController {
         
         // Configure interface objects here.
         if let recipe = context as? Recipe {
-            let rowTypes: [String] = ["RecipeHeader"]
+            let rowTypes: [String] = ["RecipeHeader"] + recipe.steps.map({ _ in "RecipeStep" })
             table.setRowTypes(rowTypes)
             
             for i in 0..<table.numberOfRows {
                 let row = table.rowControllerAtIndex(i)
                 if let header = row as? RecipeHeaderController {
                     header.titleLabel.setText(recipe.name)
+                } else if let step = row as? RecipeStepController {
+                    step.stepLabel.setText("\(i). " + recipe.steps[i - 1])
                 }
             }
         }
